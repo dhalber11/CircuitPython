3ed2121aa0bff30f78e6f967a5187efab4bd5f37
dhalber11 <dhalber11@charlottesvilleschools.org>
2022-09-12 11:00:54 -0400

    MR.H added a bunch of files

--- Neopixel.py
+++ Neopixel.py
@@ -2,9 +2,9 @@ import board
 import neopixel
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-dot.brightness = 0.5 
+dot.brightness = 0.1
 
 print("Make it red!")
 
 while True:
-    dot.fill((0, 0, 200))
+    dot.fill((255, 0, 0))
