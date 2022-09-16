aff9f2d35e3928473eddbe9fcd618881b4d82de1
dhalber11 <dhalber11@charlottesvilleschools.org>
2022-09-16 11:06:47 -0400

    Final version tweaked values in the True section

---
+++ Buttonservo.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2020 FoamyGuy for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+This example script shows how to read button state with
+debouncing that does not rely on time.sleep().
+"""
+
+import board
+from digitalio import DigitalInOut, Direction, Pull
+
+btn = DigitalInOut(board.D1)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+
+btn = DigitalInOut(board.D2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+
+prev_state = btn.value
+
+while True:
+    cur_state = btn.value
+    if cur_state != prev_state:
+        if not cur_state:
+            print("BTN is down")
+        else:
+            print("BTN is up")
+
+    prev_state = cur_state
--- Servo.py
+++ Servo.py
@@ -1,1 +1,42 @@
-#hi
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Servo standard servo example"""
+
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull
+
+btn1 = DigitalInOut(board.D1)
+btn1.direction = Direction.INPUT
+btn1.pull = Pull.DOWN
+
+btn2 = DigitalInOut(board.D2)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN 
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.A1, duty_cycle=2 ** 15, frequency=50)
+# Create a servo object, my_servo.
+
+my_servo = servo.Servo(pwm, min_pulse=600, max_pulse=2700)
+
+angle = 90 
+
+while True:
+   if btn1.value and angle <180:
+      print("btn1 pressed")
+      angle = angle +5 
+      my_servo.angle = angle 
+      time.sleep(0.01)
+   elif btn2.value and angle >0:
+      print("btn2 pressed")
+      angle = angle -5 
+      my_servo.angle = angle 
+      time.sleep(0.01)
+   else: 
+      print("servo off")
+      time.sleep(1)
